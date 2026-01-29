686763823fddde39ba3656c99b2478b85411c6dc
Yerbol Bolat <bolat.yerbol@gmail.com>
2026-01-29 20:26:16 +0500

    main changed 2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"log/slog"
 
+	"github.com/Bolatyerbol/Newgoproject/internal/config"
+	"github.com/Bolatyerbol/Newgoproject/internal/pkg/logger"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err.Error())
+	}
+
+	slogger := logger.New(cfg.LogLevel)
+	slog.SetDefault(slogger)
+
+	slog.Info("Error message")
+	slog.Debug("Debug message")
+	slog.Warn("Warn message")
+	slog.Error("Error message")
 }

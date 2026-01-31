b2dca66bbff662c636a83181d7b727725e1cd5e7
Yerbol Bolat <bolat.yerbol@gmail.com>
2026-01-31 10:10:47 +0500

    Commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"log/slog"
+	"os"
 
 	"github.com/Bolatyerbol/Newgoproject/internal/config"
 	"github.com/Bolatyerbol/Newgoproject/internal/pkg/logger"
+	"github.com/Bolatyerbol/Newgoproject/internal/server"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,8 +20,13 @@ func main() {
 	slogger := logger.New(cfg.LogLevel)
 	slog.SetDefault(slogger)
 
-	slog.Info("Error message")
-	slog.Debug("Debug message")
-	slog.Warn("Warn message")
-	slog.Error("Error message")
+	r := gin.New()
+
+	srv := server.New(r, cfg.Port)
+	err = srv.Run()
+	if err != nil {
+		slog.Error("failed to start server", "error", err.Error())
+		os.Exit(1)
+	}
+
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+type Server struct {
+	myServer *http.Server
+}
+
+func New(router http.Handler, port string) *Server {
+	return &Server{myServer: &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}}
+}
+
+func (s *Server) Run() error {
+	slog.Info("Starting server", "ADDR", s.myServer.Addr)
+	return s.myServer.ListenAndServe()
+}
